1319574550d6383742a3730bbc5e2e7744412e6c
Philippe Morier <thorgal.hayne@outlook.com>
2016-10-02 13:02:15 +0100

    refactor(trajectoryCircle): Use point for velocity

--- src/app/trajectoryCircle.ts
+++ src/app/trajectoryCircle.ts
@@ -7,8 +7,7 @@ import { Random } from './random.ts';
 export class TrajectoryCircle extends Circle implements Updatable {
     private gravity: number = 9.81;
     private timeInSeconds: number = 0;
-    private velocityX: number;
-    private velocityY: number;
+    private velocity: Point;
     private startCentre: Point;
     private alreadyResetCentreAfterBounce: boolean = false;
 
@@ -17,19 +16,21 @@ export class TrajectoryCircle extends Circle implements Updatable {
 
         this.startCentre = new Point(x, y);
 
-        this.velocityX = Random.nextFloat(0, 80);
-        this.velocityX *= Math.floor(Math.random() * 2) === 1 ? 1 : -1;
-        this.velocityY = Random.nextFloat(0, 80);
+        let velocityX: number = Random.nextFloat(0, 80);
+        velocityX *= Math.floor(Math.random() * 2) === 1 ? 1 : -1;
+        let velocityY: number = Random.nextFloat(0, 80);
+
+        this.velocity = new Point(velocityX, velocityY);
     }
 
     public update(passedMillisecondsSinceLastRendering: number): void {
         this.timeInSeconds += (passedMillisecondsSinceLastRendering / 1000);
 
-        let x: number = this.startCentre.x + this.velocityX * this.timeInSeconds;
-        let y: number = this.startCentre.y + this.velocityY * this.timeInSeconds - this.gravity / 2 * Math.pow(this.timeInSeconds, 2);
+        let x: number = this.startCentre.x + this.velocity.x * this.timeInSeconds;
+        let y: number = this.startCentre.y + this.velocity.y * this.timeInSeconds - this.gravity / 2 * Math.pow(this.timeInSeconds, 2);
 
         let isTouchingGround: boolean = (y <= this.radius);
-        let isStill: boolean = (this.velocityY < 0);
+        let isStill: boolean = (this.velocity.y < 0);
 
         if (!isTouchingGround) {
             this.centre.x = x;
@@ -37,9 +38,9 @@ export class TrajectoryCircle extends Circle implements Updatable {
             this.alreadyResetCentreAfterBounce = false;
         }
         else if (isTouchingGround && !isStill && !this.alreadyResetCentreAfterBounce) {
-            let currentVelocityY: number = this.velocityY - this.gravity * this.timeInSeconds;
+            let currentVelocityY: number = this.velocity.y - this.gravity * this.timeInSeconds;
 
-            this.velocityY = -currentVelocityY - 20;
+            this.velocity.y = -currentVelocityY - 20;
             this.timeInSeconds = 0;
             this.startCentre.x = this.centre.x;
             this.startCentre.y = this.centre.y;
