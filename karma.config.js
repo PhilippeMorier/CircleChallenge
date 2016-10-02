beeb973abdeee6f5a6837b75120dc705b1b3d34a
Philippe Morier <thorgal.hayne@outlook.com>
2016-10-02 15:53:10 +0100

    refactor(karma): Remove commented code

--- karma.config.js
+++ karma.config.js
@@ -16,40 +16,10 @@ module.exports = function (config) {
         // list of files to exclude
         exclude: [],
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        //preprocessors: {
-        //    'app.*.js': ['coverage']
-        //},
-
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['mocha'/*, 'coverage', 'karma-remap-istanbul'*/],
-
-        //coverageReporter: {
-        //    dir: '../../coverage',
-        //    reporters: [
-        //        {
-        //            type: 'text-summary'
-        //        },
-        //        {
-        //            type: 'json',
-        //            subdir: '.',
-        //            file: 'coverage-final.json'
-        //        }
-        //    ]
-        //},
-
-        //remapIstanbulReporter: {
-        //    src: './coverage/coverage-final.json',
-        //    reports: {
-        //        lcovonly: 'coverage/lcov.info',
-        //        html: 'coverage/report'
-        //    },
-        //    timeoutNotCreated: 5000,
-        //    timeoutNoMoreFiles: 1000
-        //},
 
         // web server port
         port: 9876,
@@ -67,10 +37,6 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        //singleRun: true,
 
         // Concurrency level
         // how many browser should be started simultaneous
